087b0edbfd740f66b72b4359e5c47576a95a0aff
g00gol <jackey2870@gmail.com>
2023-11-16 14:37:12 -0500

    added searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+export default function SearchBar() {
+  return (
+    <label>
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered w-full max-w-xs"
+      />
+    </label>
+  );
+}
