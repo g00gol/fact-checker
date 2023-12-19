839ade9f9b4e17963702b243efbaa47c6c10b08a
g00gol <jackeyyang.work@gmail.com>
2023-12-18 21:00:45 -0500

    asdf

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Recents from "@/components/Recents";
 
 export default function Home() {
   return (
-    <main className="mx-auto flex h-screen w-1/2 w-screen flex-col space-y-8 p-32">
+    <main className="mx-auto flex h-screen w-1/2 flex-col space-y-8 p-32">
       <h1 className="text-center text-4xl">Fact checker</h1>
       <p>
         The premise of this site is to bring awareness to misinformation online.
